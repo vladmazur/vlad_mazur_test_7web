4a0aa2701977eaebeea70a1688b120a200a587a8
Vlad Mazur <vlad@mazuru.com>
2014-01-15 18:19:46 +0200

    Add to admin filter by date and searching via titile and author

--- notesapp/admin.py
+++ notesapp/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from notesapp.models import Author, Note, Tag
 
-# class NoteAdmin(admin.ModelAdmin):
-#     fields = ['title', 'pub_date', 'author']
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['title', 'author__name']
 
 admin.site.register(Author)
-admin.site.register(Note)
+admin.site.register(Note, NoteAdmin)
 admin.site.register(Tag)
