6388212ffa62230d28c091ca115c59988580838b
Vlad Mazur <vlad@mazuru.com>
2014-01-15 18:11:25 +0200

    Add tags, some fixes

--- notesapp/admin.py
+++ notesapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from notesapp.models import Author, Note, Tag
+
+# class NoteAdmin(admin.ModelAdmin):
+#     fields = ['title', 'pub_date', 'author']
+
+admin.site.register(Author)
+admin.site.register(Note)
+admin.site.register(Tag)
--- notesapp/models.py
+++ notesapp/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
+class Tag(models.Model):
+	tag_name = 		models.CharField(max_length=35)
+	created_at  = 	models.DateTimeField('date created')
+
+	def __unicode__(self):
+		return self.tag_name
+
 class Author(models.Model):
-    # poll = models.ForeignKey(Poll)
-    name = models.CharField(max_length=200)
-    notes_count = models.IntegerField(default=0)
+    name = 	models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __unicode__ (self):
+    	return self.name
 
 class Note(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    pub_date = models.DateTimeField('date published')
-    author = models.ForeignKey(Author)
+    title = 	models.CharField(max_length=200)
+    text = 		models.TextField()
+    pub_date = 	models.DateTimeField('date published')
+    author = 	models.ForeignKey(Author)
+    tags = 		models.ManyToManyField(Tag, related_name='notes')
+
+    def __unicode__ (self):
+    	return self.title
